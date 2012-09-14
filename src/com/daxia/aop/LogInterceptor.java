e95bc72fcd9d7f181edc9fe38178bf3051ae51a4
wangshengzhong <632618392@qq.com>
2012-09-14 10:55:19 +0800

    spring aop aspectj 2

--- src/com/daxia/aop/LogInterceptor.java
+++ src/com/daxia/aop/LogInterceptor.java
@@ -5,7 +5,7 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Component //初始化
+@Component //本身需要初始化
 public class LogInterceptor {
 	@Before("execution(public void com.daxia.dao.impl.UserDAOImpl.save(com.daxia.model.User))")
 	public void beforeMethod() {
